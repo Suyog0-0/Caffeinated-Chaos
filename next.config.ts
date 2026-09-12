a98761e65fe46a0b74531606b167a7665988f7af
Suyog Sigdel <suyogsigdel000@gmail.com>
2026-09-13 00:39:30 +0545

    Allow Clearbit partner logos

--- next.config.ts
+++ next.config.ts
@@ -3,19 +3,25 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      { protocol: "https", hostname: "picsum.photos" },
-      { protocol: "https", hostname: "i.pinimg.com" },
-    {
+      {
+        protocol: "https",
+        hostname: "picsum.photos",
+      },
+      {
+        protocol: "https",
+        hostname: "logo.clearbit.com",
+      },
+      {
+        protocol: "https",
+        hostname: "i.pinimg.com",
+      },
+      {
         protocol: "https",
         hostname: "i.pravatar.cc",
       },
       {
         protocol: "https",
         hostname: "*.supabase.co",
-      },
-      {
-        protocol: "https",
-        hostname: "logo.clearbit.com",
       },
     ],
   },
