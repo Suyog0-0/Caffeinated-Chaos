16f1f7ff214c64359d38b26362022f17dcc9f64f
hacket41 <abhijit.singh.0106@gmail.com>
2026-09-12 14:29:21 +0545

    ui(metrics):metrics section added to home

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Hero } from "@/components/home/home-hero";
+import { Metrics } from "@/components/home/home-metrics";
+
+export default function Home() {
+  return (
+    <main>
+      <Hero />
+      <Metrics />
+    </main>
+  );
+}
