d1a359ea7ef3a3fb5ed22ad3ef9fda2eb8bcd82b
hacket41 <abhijit.singh.0106@gmail.com>
2026-09-12 14:36:46 +0545

    ui(area):addition to research area section to home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { Hero } from "@/components/home/home-hero";
 import { Metrics } from "@/components/home/home-metrics";
+import { ResearchAreas } from "@/components/home/home-research-areas";
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <Metrics />
+      <ResearchAreas />
     </main>
   );
 }
